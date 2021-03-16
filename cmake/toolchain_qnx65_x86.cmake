272be4bd41def1ea716d0567222f6691aaa23afe
Henryk Paluch <henryk.paluch@pickering.cz>
2021-03-16 13:23:36 +0100

    QNX version/cpu fixes for package name

--- cmake/toolchain_qnx65_x86.cmake
+++ cmake/toolchain_qnx65_x86.cmake
@@ -1,5 +1,6 @@
 # QNX cross-compiler setup for SDP 6.5.0
 set(CMAKE_SYSTEM_VERSION 6.5.0)
+set(CMAKE_SYSTEM_PROCESSOR x86)
 set(arch gcc_ntox86)
 include( ${CMAKE_CURRENT_LIST_DIR}/common_qnx.cmake )
 
